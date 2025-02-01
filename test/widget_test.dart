087c3c7cb0c0780d55c8d3cf072fcbb1dd3288b1
najib-dahir <najibdahir5666@gmail.com>
2025-02-01 23:02:11 +0300

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -14,6 +14,7 @@ import 'package:shop/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
+    
     await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
